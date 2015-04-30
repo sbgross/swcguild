e650b32b2132973082fdedd56340e4a62765c3dd
sbgross <sgross5992@outlook.com>
2015-04-30 15:18:16 -0400

    Initial push.

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i < 101; i++)
+            {
+                string result = i + " ";
+                if (i % 3 == 0)
+                {
+                    result += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    result += "Buzz";
+                }
+
+                Console.WriteLine(result);             
+            }
+            Console.ReadLine();
+        }
+    }
+}
