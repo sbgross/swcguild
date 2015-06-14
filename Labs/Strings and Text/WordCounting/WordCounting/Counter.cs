f49cb55c3b983f6dafe66177f443c838f34d5f7d
sbgross <sgross5992@outlook.com>
2015-06-14 12:06:28 -0400

    Initial push.

---
+++ Labs/Strings and Text/WordCounting/WordCounting/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounting
+{
+    public class Counter
+    {
+        public int CountWords(string countThis)
+        {
+            int wordCount = 0;
+
+            string[] split = countThis.Split(new Char[] { ' ', ',', '.', ':', '\t' });
+
+            foreach (var w in split)
+            {
+                wordCount++;
+            }
+
+            return wordCount;
+        }
+    }
+}
---
+++ Labs/Strings and Text/WordCounting/WordCounting/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounting
+{
+    public class MainMenu
+    {
+        public void Execute()
+        {
+            do
+            {
+                Console.WriteLine("Word Counting Program");
+                Console.WriteLine("==========================");
+                Console.WriteLine("This program counts the number of words in a phrase.");
+
+                Console.Write("\nEnter a word or phrase: ");
+                string countThis = Console.ReadLine().ToLower();
+
+                Counter c = new Counter();
+                int wordCount = c.CountWords(countThis);
+
+                Console.WriteLine("\nThe number of words is {0}.", wordCount);
+
+                Console.Write("\nWould you like to enter another word of phrase? ");
+                var userInput = Console.ReadLine().ToUpper();
+
+                if (userInput == "N")
+                {
+                    break;
+                }
+
+                Console.Clear();
+
+            } while (true);
+        }
+    }
+}
